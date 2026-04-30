e929acecceda90bbe70a085ab7155a11bc238015
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-04-30 13:25:11 +0530

    Update SecureMultiVendorECommerceMarketplacePlatformApplication.java

--- backend/src/main/java/com/example.securemultivendorecommercemarketplaceplatfrom/SecureMultiVendorECommerceMarketplacePlatformApplication.java
+++ backend/src/main/java/com/example.securemultivendorecommercemarketplaceplatfrom/SecureMultiVendorECommerceMarketplacePlatformApplication.java
@@ -3,10 +3,16 @@ package com.example.securemultivendorecommercemarketplaceplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.securemultivendorecommercemarketplaceplatform")
+@SpringBootApplication(
+    scanBasePackages = "com.example.securemultivendorecommercemarketplaceplatform",
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+    }
+)
 public class SecureMultiVendorECommerceMarketplacePlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecureMultiVendorECommerceMarketplacePlatformApplication.class, args);
     }
 }
+
